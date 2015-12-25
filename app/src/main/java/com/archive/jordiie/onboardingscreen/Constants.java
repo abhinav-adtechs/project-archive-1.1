b5c78d4b1633062f0320c85196071f2dad82a08f
abhinav-adtechs <abhinav.adtechs@gmail.com>
2015-12-25 17:22:46 +0530

    Retrofit revert. Volley init.

---
+++ app/src/main/java/com/archive/jordiie/onboardingscreen/Constants.java
@@ -0,0 +1,14 @@
+package com.archive.jordiie.onboardingscreen;
+
+/**
+ * Created by jordiie on 25/12/15.
+ */
+public class Constants {
+    public static final int SERVERTEST_CODE=0;
+    public static final int LOGIN_CODE=1;
+    public static final int REFRESH_CODE=2;
+    public static final int GENERIC_CODE=-1;
+    public static final int GET = 1 ;
+
+
+}
